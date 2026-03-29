75ee50b6995556e820e5d5c8c09c84a7da6d290d
nikki-786 <nikitasahu5113@gmail.com>
2026-03-29 17:19:47 +0530

    Time: 2 ms (29.59%) | Memory: 44.1 MB (68.88%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,9 +1,15 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        if(s1.charAt(0)!=s2.charAt(0)&&(s1.charAt(0)!=s2.charAt(2)||s1.charAt(2)!=s2.charAt(0)))return false;
-        if(s1.charAt(1)!=s2.charAt(1)&&(s1.charAt(1)!=s2.charAt(3)||s1.charAt(3)!=s2.charAt(1)))return false;
-        if(s1.charAt(0)==s2.charAt(0)&&s1.charAt(2)!=s2.charAt(2))return false;
-        if(s1.charAt(1)==s2.charAt(1)&&s1.charAt(3)!=s2.charAt(3))return false;
-        return true;
+        char[] even1 = {s1.charAt(0), s1.charAt(2)};
+        char[] even2 = {s2.charAt(0), s2.charAt(2)};
+        char[] odd1 = {s1.charAt(1), s1.charAt(3)};
+        char[] odd2 = {s2.charAt(1), s2.charAt(3)};
+        
+        Arrays.sort(even1);
+        Arrays.sort(even2);
+        Arrays.sort(odd1);
+        Arrays.sort(odd2);
+        
+        return Arrays.equals(even1, even2) && Arrays.equals(odd1, odd2);
     }
 }
