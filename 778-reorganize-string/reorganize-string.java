9490d26926328c536e361523f98c1230773380bd
nikki-786 <nikitasahu5113@gmail.com>
2026-05-21 12:15:57 +0530

    Time: 7 ms (22.37%) | Memory: 43.2 MB (26.05%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String reorganizeString(String s) {
+
+        // Count frequency of characters
+        Map<Character, Integer> freq = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        // Max Heap based on frequency
+        PriorityQueue<Character> pq =
+                new PriorityQueue<>(
+                        (a, b) -> freq.get(b) - freq.get(a)
+                );
+
+        pq.addAll(freq.keySet());
+
+        StringBuilder ans = new StringBuilder();
+
+        // Store previous character temporarily
+        Character prev = null;
+
+        while (!pq.isEmpty()) {
+
+            // Get character with highest frequency
+            char curr = pq.poll();
+
+            // Add current character
+            ans.append(curr);
+
+            // Reduce frequency
+            freq.put(curr, freq.get(curr) - 1);
+
+            // Add previous character back if still available
+            if (prev != null && freq.get(prev) > 0) {
+                pq.add(prev);
+            }
+
+            // Current character becomes previous
+            prev = curr;
+        }
+
+        // Check if valid rearrangement was formed
+        if (ans.length() != s.length()) {
+            return "";
+        }
+
+        return ans.toString(); 
+    }
+}
