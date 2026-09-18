7cfcce4ec8976d034a65dbd37c6b9b1fee30769d
nikki-786 <nikitasahu5113@gmail.com>
2026-09-18 21:20:24 +0530

    Time: 15 ms (12.46%) | Memory: 159.9 MB (48.71%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,22 +1,40 @@
+
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int count = 0;
-        for(int ele : nums){
-            if(ele <= pivot) count++;
-        }
-        int[] ans = new int[nums.length]; 
-        int i = 0;
-        Arrays.fill(ans,pivot);
-        for(int j = 0 ; j < nums.length ; j++){
-            if(nums[j] < pivot){
-                ans[i++] = nums[j];
-            }
-            if(nums[j] > pivot){
-                ans[count++] = nums[j];
-            }
-        }
-        return ans;
+// The final array size equals nums.length (all elements go somewhere)
+int[] result = new int[nums.length];
+List<Integer> less = new ArrayList<>();
+List<Integer> equal = new ArrayList<>();
+List<Integer> greater = new ArrayList<>();
+        for (int n : nums) {
+    if (n < pivot) {
+        less.add(n);
+    } else if (n == pivot) {
+        // ___ ?
+        equal.add(n);
+    } else {
+        // ___ ?
+        greater.add(n);
+    }
+}
+// After categorization loop...
 
-        
+int idx = 0;
+
+// Copy all three lists into result array
+for (int val : less) {
+    result[idx++] = val;
+}
+
+for (int val : equal) {
+    result[idx++] = val;
+}
+
+for (int val : greater) {
+    // ___ ?
+    result[idx++] = val;
+}
+
+return result;
     }
 }
