949ddab053371aad933bf0f0e162d3d9fb76540a
nikki-786 <nikitasahu5113@gmail.com>
2026-09-21 20:23:33 +0530

    Time: 4 ms (99.76%) | Memory: 160 MB (48.92%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,40 +1,26 @@
-
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-// The final array size equals nums.length (all elements go somewhere)
-int[] result = new int[nums.length];
-List<Integer> less = new ArrayList<>();
-List<Integer> equal = new ArrayList<>();
-List<Integer> greater = new ArrayList<>();
-        for (int n : nums) {
-    if (n < pivot) {
-        less.add(n);
-    } else if (n == pivot) {
-        // ___ ?
-        equal.add(n);
-    } else {
-        // ___ ?
-        greater.add(n);
-    }
-}
-// After categorization loop...
+        int[] result = new int[nums.length];
+        int index = 0;
 
-int idx = 0;
+        for (int num : nums) {
+            if (num < pivot) {
+                result[index++] = num;
+            }
+        }
 
-// Copy all three lists into result array
-for (int val : less) {
-    result[idx++] = val;
-}
+        for (int num : nums) {
+            if (num == pivot) {
+                result[index++] = num;
+            }
+        }
 
-for (int val : equal) {
-    result[idx++] = val;
-}
+        for (int num : nums) {
+            if (num > pivot) {
+                result[index++] = num;
+            }
+        }
 
-for (int val : greater) {
-    // ___ ?
-    result[idx++] = val;
-}
-
-return result;
+        return result;
     }
 }
